90f57e6810596ef326d78de06f7b660d7c608704
rolindaz <rolinda.zamboni@gmail.com>
2025-05-27 15:15:10 +0200

    Implement travel cost calculation with age-based discounts

--- js/script.js
+++ js/script.js
@@ -16,13 +16,34 @@ TIP: per controllare che la vostra logica sui prezzi funzioni correttamente, pro
 
 // chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
 
+const userKm = Number(prompt('Quanti km vuoi percorrere?')), userAge = Number(prompt('Quanti anni hai?'));
 
+console.log(userKm, userAge);
 
-// prezzo totale del viaggio, secondo queste regole: 
 // - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+let basePrice = userKm * 0.21;
+
+console.log(`Prezzo base: ${basePrice.toFixed(2)}`);
+
 // - va applicato uno sconto del 20% per i minorenni
+
+let youngDiscount = basePrice / 100 * 20;
+
 // - va applicato uno sconto del 40% per gli over 65
 
+let elderDiscount = basePrice / 100 * 40;
 
+// prezzo totale del viaggio, secondo queste regole: 
 
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
+let totalPriceYoung = basePrice - youngDiscount, totalPriceElder = basePrice - elderDiscount;
+
+if (userAge < 18) {
+    console.log(`Sconto per under 18: ${youngDiscount.toFixed(2)} €`)
+    console.log(`Prezzo totale biglietto: ${totalPriceYoung.toFixed(2)} €`)
+} else if (userAge >= 65) {
+    console.log(`Sconto per over 65: ${elderDiscount.toFixed(2)} €`)
+    console.log(`Prezzo totale biglietto: ${totalPriceElder.toFixed(2)} €`)
+} else {
+    console.log(`Prezzo totale biglietto: ${basePrice.toFixed(2)} €`)
+};
